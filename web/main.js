3e523ef60927a62d127f8db92f1b5b5ab86c4f7b
gene cai <jin.cai@standard.com>
2021-07-21 06:24:29 -0700

    update UI

--- web/main.js
+++ web/main.js
@@ -9,3 +9,12 @@ async function create_new_workbook() {
 async function get_question() {
     await eel.get_question()();    
 }
+
+function toggleSetting(){
+    cur_setting = document.getElementById("setting").innerHTML
+    if (cur_setting == "Spring"){
+        document.getElementById("setting").innerHTML = "Fall"
+    }else{
+        document.getElementById("setting").innerHTML = "Spring"
+    }
+}
