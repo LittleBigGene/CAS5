8c0e9371eda0b31410cdd2808ff16f1f09f92a55
gene cai <jin.cai@standard.com>
2021-07-21 08:54:09 -0700

    input validation

--- web/main.js
+++ web/main.js
@@ -1,20 +1,28 @@
-
-
 async function create_new_workbook() {
     document.getElementById("status").innerHTML = "Opening Excel...";
-    result = await eel.create_new_workbook()();    
-    document.getElementById("status").innerHTML = "";    
+    result = await eel.create_new_workbook()();
+    document.getElementById("status").innerHTML = "";
 }
 
 async function get_question() {
-    await eel.get_question()();    
+    alert(get_form_input())
+    // await eel.get_question()();
 }
 
-function toggleSetting(){
+function get_form_input() {
+    setting = document.getElementById("setting").innerText
+    year = document.getElementById("year").value
+    exam = document.getElementById("exam").value.toUpperCase()
+    q = document.getElementById("question").value
+
+    return `${setting}${year}${exam}#${q}`
+}
+
+function toggleSetting() {
     cur_setting = document.getElementById("setting").innerHTML
-    if (cur_setting == "Spring"){
-        document.getElementById("setting").innerHTML = "Fall"
-    }else{
-        document.getElementById("setting").innerHTML = "Spring"
+    if (cur_setting == "Spring") {
+        document.getElementById("setting").innerText = "Fall"
+    } else {
+        document.getElementById("setting").innerText = "Spring"
     }
 }
